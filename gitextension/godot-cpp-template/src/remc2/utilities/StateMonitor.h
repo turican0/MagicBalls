97e94238373ef68d125302b52330826ac8eadaa1
Tomáš Veselý <turican0@gmail.com>
2025-12-28 12:40:18 +0100

    update state monitor

--- gitextension/godot-cpp-template/src/remc2/utilities/StateMonitor.h
+++ gitextension/godot-cpp-template/src/remc2/utilities/StateMonitor.h
@@ -1,7 +1,9 @@
 #pragma once
-
-//#include <SDL2/SDL.h>
-//#include <SDL2/SDL_ttf.h>
+#include "../build_config.h"
+#ifdef REMC2_CODE
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#endif //REMC2_CODE
 #include <string>
 #include <map>
 
@@ -44,14 +46,14 @@ class StateMonitor {
         void Monitor17DB70str();
         void Monitor_str_D4C48ar();
 
-        //bool m_initialized = false;
-
-        //TTF_Font *m_font;
-        //SDL_Window *m_window;
-        //SDL_Renderer *m_renderer;
-        //SDL_Surface *m_surface;
-        //SDL_Texture *m_texture;
-
+        bool m_initialized = false;
+#ifdef REMC2_CODE
+        TTF_Font *m_font;
+        SDL_Window *m_window;
+        SDL_Renderer *m_renderer;
+        SDL_Surface *m_surface;
+        SDL_Texture *m_texture;
+#endif //REMC2_CODE
         map<string, ObservedChange> m_observed_changes;
 
         LastStates m_last_states;
