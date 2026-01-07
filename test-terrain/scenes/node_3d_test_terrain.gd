33fbcd059e8a27dad7aa86be983b7bc020481b20
Tomaš Veselý <turican0@gmail.com>
2026-01-07 09:24:50 +0100

    add code

--- test-terrain/scenes/node_3d_test_terrain.gd
+++ test-terrain/scenes/node_3d_test_terrain.gd
@@ -141,6 +141,8 @@ func recalculate_mesh():
 	surface_tool = SurfaceTool.new()
 	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
 	
+	#surface_tool.set_custom_format(0, SurfaceTool.CUSTOM_RGBA_FLOAT)
+	
 	wave_scale.resize(GRID_SIZE)
 	for x in range(GRID_SIZE):
 		wave_scale[x] = []
@@ -168,27 +170,39 @@ func recalculate_mesh():
 			var waves3 = wave_scale[(x+1)%GRID_SIZE][(y+1)%GRID_SIZE]
 			var waves4 = wave_scale[x][(y+1)%GRID_SIZE]
 			
+			var g1 = Vector2(x, y)
+			var g2 = Vector2(x+1, y)
+			var g3 = Vector2(x+1, y+1)
+			var g4 = Vector2(x, y+1)
+			
 			var texture_index = texture_indices[x][y]
 			if ((x+y+1)&1):
-				add_triangle(v1, v2, v3, texture_index,rPoint1,rPoint2,rPoint3,waves1,waves2,waves3)
-				add_triangle(v1, v3, v4, texture_index,rPoint1,rPoint3,rPoint4,waves1,waves3,waves4)
+				add_triangle(v1, v2, v3, texture_index,rPoint1,rPoint2,rPoint3,waves1,waves2,waves3, g1, g2, g3)
+				add_triangle(v1, v3, v4, texture_index,rPoint1,rPoint3,rPoint4,waves1,waves3,waves4, g1, g3, g4)
 			else:
-				add_triangle(v2, v3, v4, texture_index,rPoint2,rPoint3,rPoint4,waves2,waves3,waves4)
-				add_triangle(v2, v4, v1, texture_index,rPoint2,rPoint4,rPoint1,waves2,waves4,waves1)
+				add_triangle(v2, v3, v4, texture_index,rPoint2,rPoint3,rPoint4,waves2,waves3,waves4, g2, g3, g4)
+				add_triangle(v2, v4, v1, texture_index,rPoint2,rPoint4,rPoint1,waves2,waves4,waves1, g2, g4, g1)
 	surface_tool.generate_normals()
 	surface_tool.index()
 	mesh_instance.mesh = surface_tool.commit()
 
-func add_triangle(p1: Vector3, p2: Vector3, p3: Vector3, idx1: int, uv1: Vector2, uv2: Vector2, uv3: Vector2, w1_1:float, w1_2:float, w1_3:float):
+func add_triangle(p1: Vector3, p2: Vector3, p3: Vector3, idx1: int, uv1: Vector2, uv2: Vector2, uv3: Vector2, w1_1:float, w1_2:float, w1_3:float, grid_p1: Vector2, grid_p2: Vector2, grid_p3: Vector2):
 	var verts = [p1, p2, p3]
 	var uvs = [uv1, uv2, uv3]
 	
 	var wave_sizes1 = [w1_1, w1_2, w1_3]
 	
+	var global_uvs = [
+		grid_p1 / float(GRID_SIZE),
+		grid_p2 / float(GRID_SIZE),
+		grid_p3 / float(GRID_SIZE)
+	]
+	
 	for i in range(3):
 		surface_tool.set_color(Color(0, wave_sizes1[i], 0))
 		surface_tool.set_uv(uvs[i])
 		surface_tool.set_uv2(Vector2(idx1, 0))
+		#surface_tool.set_custom(0, Color(global_uvs[i].x, global_uvs[i].y, 0, 0))
 		surface_tool.add_vertex(verts[i])
 
 # Funkce, která by se volala, kdykoli se změní výška:
