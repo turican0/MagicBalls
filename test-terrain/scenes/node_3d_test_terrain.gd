577d1747f1d9442c6d348b52b18799cc48648644
Tomaš Veselý <turican0@gmail.com>
2026-01-08 07:16:33 +0100

    fix shader waves

--- test-terrain/scenes/node_3d_test_terrain.gd
+++ test-terrain/scenes/node_3d_test_terrain.gd
@@ -184,17 +184,17 @@ func recalculate_mesh():
 			
 			var texture_index = texture_indices[x][y]
 			if ((x+y+1)&1):
-				add_triangle(v1, v2, v3, texture_index,rPoint1,rPoint2,rPoint3,g1, g2, g3,x,y)
-				add_triangle(v1, v3, v4, texture_index,rPoint1,rPoint3,rPoint4,g1, g3, g4,x,y)
+				add_triangle(v1, v2, v3, texture_index,rPoint1,rPoint2,rPoint3,g1, g2, g3, g1)
+				add_triangle(v1, v3, v4, texture_index,rPoint1,rPoint3,rPoint4,g1, g3, g4, g1)
 			else:
-				add_triangle(v2, v3, v4, texture_index,rPoint2,rPoint3,rPoint4,g2, g3, g4,x,y)
-				add_triangle(v2, v4, v1, texture_index,rPoint2,rPoint4,rPoint1,g2, g4, g1,x,y)
+				add_triangle(v2, v3, v4, texture_index,rPoint2,rPoint3,rPoint4,g2, g3, g4, g1)
+				add_triangle(v2, v4, v1, texture_index,rPoint2,rPoint4,rPoint1,g2, g4, g1, g1)
 	surface_tool.generate_normals()
 	surface_tool.index()
 	mesh_instance.mesh = surface_tool.commit()
 	renew_terrain()
 
-func add_triangle(p1: Vector3, p2: Vector3, p3: Vector3, idx1: int, uv1: Vector2, uv2: Vector2, uv3: Vector2, grid_p1: Vector2, grid_p2: Vector2, grid_p3: Vector2, x:int, y:int):
+func add_triangle(p1: Vector3, p2: Vector3, p3: Vector3, idx1: int, uv1: Vector2, uv2: Vector2, uv3: Vector2, grid_p1: Vector2, grid_p2: Vector2, grid_p3: Vector2, main_p: Vector2):
 	var verts = [p1, p2, p3]
 	var uvs = [uv1, uv2, uv3]
 	
@@ -204,11 +204,13 @@ func add_triangle(p1: Vector3, p2: Vector3, p3: Vector3, idx1: int, uv1: Vector2
 		grid_p3 / float(GRID_SIZE)
 	]
 	
+	var main_uvs = main_p / float(GRID_SIZE)
+	
 	for i in range(3):
 		#surface_tool.set_color(Color(0, wave_sizes1[i], 0))
 		surface_tool.set_uv(uvs[i])
 		#surface_tool.set_uv2(Vector2(idx1, 0))
-		surface_tool.set_custom(0, Color(global_uvs[i].x, global_uvs[i].y, x, y))
+		surface_tool.set_custom(0, Color(global_uvs[i].x, global_uvs[i].y, main_uvs.x, main_uvs.y))
 		surface_tool.add_vertex(verts[i])
 
 # Funkce, která by se volala, kdykoli se změní výška:
