bf78584c3ba28a854b42916284f434432a69d1e3
Tomaš Veselý <turican0@gmail.com>
2026-02-18 12:06:51 +0100

    fix hide minimap

--- godot-code/scenes/UI.gd
+++ godot-code/scenes/UI.gd
@@ -61,53 +61,53 @@ func update_hover_selection():
 			break
 
 func _process(_p_delta) -> void:
-	$Label.text = "FPS: %d\n" % Engine.get_frames_per_second()
-	if(visible_mode == 1):
-		$Label.text += "Move Speed: %.1f\n" % player.MOVE_SPEED if player else ""
-		$Label.text += "Life: %.1f\n" % player.LIFE if player else ""
-		$Label.text += "Mana: %.1f\n" % player.MANA if player else ""
-		$Label.text += "Position: %.1v\n" % player.global_position if player else ""
-		$Label.text += """
-			Player
-			Move: WASDEQ,Space,Mouse
-			Move speed: Wheel,+/-,Shift
-			Camera View: V
-			Gravity toggle: G
-			Collision toggle: C
-
-			Window
-			Quit: F8
-			UI toggle: F9
-			Render mode: F10
-			Full screen: F11
-			Mouse toggle: Escape / F12
-			"""
+	#$Label.text = "FPS: %d\n" % Engine.get_frames_per_second()
+	#if(visible_mode == 1):
+		#$Label.text += "Move Speed: %.1f\n" % player.MOVE_SPEED if player else ""
+		#$Label.text += "Life: %.1f\n" % player.LIFE if player else ""
+		#$Label.text += "Mana: %.1f\n" % player.MANA if player else ""
+		#$Label.text += "Position: %.1v\n" % player.global_position if player else ""
+		#$Label.text += """
+			#Player
+			#Move: WASDEQ,Space,Mouse
+			#Move speed: Wheel,+/-,Shift
+			#Camera View: V
+			#Gravity toggle: G
+			#Collision toggle: C
+#
+			#Window
+			#Quit: F8
+			#UI toggle: F9
+			#Render mode: F10
+			#Full screen: F11
+			#Mouse toggle: Escape / F12
+			#"""
 	if is_ctrl_active:
 		update_hover_selection()
 
 
-func _unhandled_key_input(p_event: InputEvent) -> void:
-	if p_event is InputEventKey and p_event.pressed:
-		match p_event.keycode:
-			KEY_F8:
-				get_tree().quit()
-			KEY_F9:
-				visible_mode = (visible_mode + 1 ) % 3
-				$Label/Panel.visible = (visible_mode == 1)
-				visible = visible_mode > 0
-			KEY_F10:
-				var vp = get_viewport()
-				vp.debug_draw = (vp.debug_draw + 1 ) % 6
-				get_viewport().set_input_as_handled()
-			KEY_F11:
-				toggle_fullscreen()
-				get_viewport().set_input_as_handled()
-			KEY_ESCAPE, KEY_F12:
-				if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
-					Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-				else:
-					Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-				get_viewport().set_input_as_handled()
+#func _unhandled_key_input(p_event: InputEvent) -> void:
+	#if p_event is InputEventKey and p_event.pressed:
+		#match p_event.keycode:
+			#KEY_F8:
+				#get_tree().quit()
+			#KEY_F9:
+				#visible_mode = (visible_mode + 1 ) % 3
+				#$Label/Panel.visible = (visible_mode == 1)
+				#visible = visible_mode > 0
+			#KEY_F10:
+				#var vp = get_viewport()
+				#vp.debug_draw = (vp.debug_draw + 1 ) % 6
+				#get_viewport().set_input_as_handled()
+			#KEY_F11:
+				#toggle_fullscreen()
+				#get_viewport().set_input_as_handled()
+			#KEY_ESCAPE, KEY_F12:
+				#if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+					#Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+				#else:
+					#Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+				#get_viewport().set_input_as_handled()
 		
 		
 func toggle_fullscreen() -> void:
