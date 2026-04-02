ff9241062c1f86d84b24853bc0e94326c6634c25
Tomaš Veselý <turican0@gmail.com>
2026-04-02 08:28:20 +0200

    update engine support

--- gitextension/godot-cpp-template/src/remc2/engine/engine_support.cpp
+++ gitextension/godot-cpp-template/src/remc2/engine/engine_support.cpp
@@ -2033,85 +2033,16 @@ inline void setRGBA(png_byte* ptr, uint8_t* val)
 #endif //REMC2_CODE
 
 #ifdef _DEBUG
+#include "../lib/stb_image_write.h"
 int writeImage(const char* filename, int width, int height, uint8_t* buffer, char* title)
 {
-	int code = 0;
-	FILE* fp = NULL;
-	png_structp png_ptr = NULL;
-	png_infop info_ptr = NULL;
-	png_bytep row = NULL;
-
-	// Open file for writing (binary mode)
-	fp = fopen(filename, "wb");
-	if (fp == NULL) {
-		Logger->error("Could not open file %s for writing {}", filename);
-		code = 1;
-		goto finalise;
+	int success = stbi_write_png(filename, width, height, 4, buffer, width * 4);
+	if (success == 0) {
+		Logger->error("Could not write PNG file {}", filename);
+		return 1;
 	}
 
-	// Initialize write structure
-	png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-	if (png_ptr == NULL) {
-		Logger->error("Could not allocate write struct");
-		code = 1;
-		goto finalise;
-	}
-
-	// Initialize info structure
-	info_ptr = png_create_info_struct(png_ptr);
-	if (info_ptr == NULL) {
-		Logger->error("Could not allocate info struct");
-		code = 1;
-		goto finalise;
-	}
-
-	// Setup Exception handling
-	if (setjmp(png_jmpbuf(png_ptr))) {
-		Logger->error("Error during png creation");
-		code = 1;
-		goto finalise;
-	}
-
-	png_init_io(png_ptr, fp);
-
-	// Write header (8 bit colour depth)
-	png_set_IHDR(png_ptr, info_ptr, width, height,
-		8, PNG_COLOR_TYPE_RGBA, PNG_INTERLACE_NONE,
-		PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-
-	// Set title
-	if (title != NULL) {
-		png_text title_text;
-		title_text.compression = PNG_TEXT_COMPRESSION_NONE;
-		title_text.key = (png_charp)"Title";
-		title_text.text = title;
-		png_set_text(png_ptr, info_ptr, &title_text, 1);
-	}
-
-	png_write_info(png_ptr, info_ptr);
-
-	// Allocate memory for one row (3 bytes per pixel - RGB)
-	row = (png_bytep)malloc(4 * width * sizeof(png_byte));
-
-	// Write image data
-	int x, y;
-	for (y = 0; y < height; y++) {
-		for (x = 0; x < width; x++) {
-			setRGBA(&(row[x * 4]), buffer + (y * width + x) * 4);
-		}
-		png_write_row(png_ptr, row);
-	}
-
-	// End write
-	png_write_end(png_ptr, NULL);
-
-finalise:
-	if (fp != NULL) fclose(fp);
-	if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
-	if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
-	if (row != NULL) free(row);
-
-	return code;
+	return 0; // OK
 }
 #endif
 
