4d08c56833cff6aac80cdc13f05ad2f5c4756dee
Tomaš Veselý <turican0@gmail.com>
2026-04-13 14:09:12 +0200

    update

--- mytools/decodeXMI2/decodeXMI2/decodeXMI2.cpp
+++ mytools/decodeXMI2/decodeXMI2/decodeXMI2.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <cstring>   // pro memcpy
 #include <cstdio>
+#include <vector>
 
 __int16 m_iNumberOfTracks = 0;
 int countOfMusicTracks_E380C = 0;
@@ -168,30 +169,87 @@ void GetMusicSequenceCount() {
 uint8_t* Zero_pointer = NULL;
 int filelenght;
 
-bool LoadMusicTrack(uint8_t* fullMusicData, type_v8 headerx[4], uint8_t drivernumber) {
-    if (headerx[drivernumber].dword_4 == -1) return false;
+bool LoadMusicTrack(FILE* filehandle,uint8_t* fullMusicData, type_v8 headerx[4], uint8_t drivernumber) {
+    int rncsize;
 
-    if (musicData_E3810) free(musicData_E3810);
-    if (musicHeader_E3808) free((uint8_t*)musicHeader_E3808);
-
+    ftell(filehandle);
+    fread((uint8_t*)headerx, 1, 64, filehandle);
+    //DataFileIO::Read(filehandle, (uint8_t*)headerx, 64);
+    if (headerx[drivernumber].dword_4 == -1)
+        return false;
+    if (musicData_E3810)
+    {
+        //FreeMem_83E80(musicData_E3810);
+        //musicAble_E37FC = false;
+    }
+    if (musicHeader_E3808)
+    {
+        //FreeMem_83E80((uint8_t*)musicHeader_E3808);
+        //musicAble_E37FC = false;
+    }
     musicData_E3810 = (uint8_t*)malloc(headerx[drivernumber].dword_12);
-    if (!musicData_E3810) return false;
-
+    if (!musicData_E3810)
+        return false;
     musicHeader_E3808 = (type_E3808_music_header*)malloc(sizeof(type_E3808_music_header));
-    if (!musicHeader_E3808) { free(musicData_E3810); return false; }
+    if (!musicHeader_E3808)
+    {
+        //FreeMem_83E80(musicData_E3810);
+        return false;
+    }
 
+    //64xbit fix
     shadow_type_E3808_music_header* shadow_str_E3808_music_header = (shadow_type_E3808_music_header*)malloc(sizeof(shadow_type_E3808_music_header));
-    if (!shadow_str_E3808_music_header) { free(musicData_E3810); free((uint8_t*)musicHeader_E3808); return false; }
+    if (!shadow_str_E3808_music_header)
+    {
+        //FreeMem_83E80((uint8_t*)shadow_str_E3808_music_header);
+        return false;
+    }
+    //64xbit fix
 
-    uint32_t musicDataOffset = headerx[drivernumber].dword_4;
-    uint32_t musicDataSize = headerx[drivernumber].dword_12;
-    memcpy(musicData_E3810, fullMusicData + musicDataOffset, musicDataSize);
+    countOfMusicTracks_E380C = (headerx[drivernumber].sizeBytes_8) / sizeof(sub2type_E37A0_sound_buffer2);
+    fseek(filehandle, headerx[drivernumber].dword_4, 0);
+    fread(musicData_E3810, 1, 8, filehandle);
+    //DataFileIO::Read(filehandle, musicData_E3810, 8);
+    /*if (musicData_E3810[0] == 'R' && musicData_E3810[1] == 'N' && musicData_E3810[2] == 'C')//RNC
+    {
+        rncsize = musicData_E3810[4];
+        rncsize <<= 8;
+        rncsize += musicData_E3810[5];
+        rncsize <<= 8;
+        rncsize += musicData_E3810[6];
+        rncsize <<= 8;
+        rncsize += musicData_E3810[7];
+        fread((uint8_t*)&musicData_E3810[8], 1, rncsize - 8, filehandle);
+        //DataFileIO::Read(filehandle, (uint8_t*)&musicData_E3810[8], rncsize - 8);
+        DataFileRNC::Decompress(musicData_E3810, musicData_E3810);
+    }
+    else*/
+    {
+        fread((uint8_t*)&musicData_E3810[8], 1, headerx[drivernumber].dword_12 - 8, filehandle);
+        //DataFileIO::Read(filehandle, (uint8_t*)&musicData_E3810[8], headerx[drivernumber].dword_12 - 8);
+    }
+    fseek(filehandle, headerx[drivernumber].dword_0, 0);
 
-    uint32_t musicHeaderOffset = headerx[drivernumber].dword_0;
-    uint32_t musicHeaderSize = headerx[drivernumber].sizeBytes_8;
-    memcpy(shadow_str_E3808_music_header, fullMusicData + musicHeaderOffset, 8);
-    memcpy(&(shadow_str_E3808_music_header->str_8), fullMusicData + musicHeaderOffset + 8, musicHeaderSize - 8);
-
+    //64x fix shadow_str_E3808_music_header
+    fread((uint8_t*)shadow_str_E3808_music_header, 1, 8, filehandle);
+    //DataFileIO::Read(filehandle, (uint8_t*)shadow_str_E3808_music_header, 8);
+    /*if (shadow_str_E3808_music_header->byte_0 == 'R' && shadow_str_E3808_music_header->byte_1 == 'N' && shadow_str_E3808_music_header->byte_2 == 'C')//RNC
+    {
+        rncsize = shadow_str_E3808_music_header->byte_4;
+        rncsize <<= 8;
+        rncsize += shadow_str_E3808_music_header->byte_5;
+        rncsize <<= 8;
+        rncsize += shadow_str_E3808_music_header->byte_6;
+        rncsize <<= 8;
+        rncsize += shadow_str_E3808_music_header->byte_7;
+        DataFileIO::Read(filehandle, (uint8_t*)(&shadow_str_E3808_music_header->str_8), rncsize - 8);
+        DataFileRNC::Decompress((uint8_t*)shadow_str_E3808_music_header, (uint8_t*)shadow_str_E3808_music_header);
+    }
+    else*/
+    {
+        fread((uint8_t*)(&shadow_str_E3808_music_header->str_8), 1, headerx[drivernumber].sizeBytes_8 - 8, filehandle);
+        //DataFileIO::Read(filehandle, (uint8_t*)(&shadow_str_E3808_music_header->str_8), headerx[drivernumber].sizeBytes_8 - 8);
+    }
     musicHeader_E3808->byte_0 = shadow_str_E3808_music_header->byte_0;
     musicHeader_E3808->byte_1 = shadow_str_E3808_music_header->byte_1;
     musicHeader_E3808->byte_2 = shadow_str_E3808_music_header->byte_2;
@@ -214,7 +272,6 @@ bool LoadMusicTrack(uint8_t* fullMusicData, type_v8 headerx[4], uint8_t drivernu
     }
     for (int i = 0; i < 14; i++)
         musicHeader_E3808->str_8.stubb[i] = shadow_str_E3808_music_header->str_8.stubb[i];
-    free((uint8_t*)shadow_str_E3808_music_header);
 
     GetMusicSequenceCount();
 
@@ -222,8 +279,44 @@ bool LoadMusicTrack(uint8_t* fullMusicData, type_v8 headerx[4], uint8_t drivernu
     trackType track[6];
     for (int i = 1; i <= m_iNumberOfTracks; i++) {
         track[i - 1].begin = musicHeader_E3808->str_8.track_10[i - 1].xmiData_0;
-        track[i - 1].end = track[i - 1].begin + musicHeader_E3808->str_8.track_10[i - 1].xmiSize_8;
+        track[i - 1].end = track[i - 1].begin + (uint16_t)musicHeader_E3808->str_8.track_10[i - 1].xmiSize_8;
+		FILE* outFile = nullptr;
+        fopen_s(&outFile,(char*)musicHeader_E3808->str_8.track_10[i - 1].filename_14,"wb");
+		fwrite(track[i - 1].begin, 1, track[i - 1].end - track[i - 1].begin, outFile);
+        fclose(outFile);
     }
+
+    const char* pattern = "XDIRINFO";
+    size_t patternLen = strlen(pattern);
+    long positions[50];
+    //if (filelenght < patternLen) return;
+    int index = 0;
+    for (size_t i = 0; i <= filelenght - patternLen; ++i) {
+        if (memcmp(&fullMusicData[i], pattern, patternLen) == 0) {
+            positions[index]=i;
+            index++;
+        }
+    }
+
+    for (int i = 0; i < index; i++) {
+        long pos0 = positions[i] - 8;
+        uint8_t* begin = &fullMusicData[pos0];
+        uint8_t* end;
+        if (i < index - 1)
+        {
+            long pos1 = positions[i+1] - 8;
+            end = &fullMusicData[pos1];
+        }
+        else
+            end = &fullMusicData[filelenght];
+        FILE* outFile = nullptr;
+        char path[512];
+        sprintf_s(path, "xmifile%2d.XMI", i);
+        fopen_s(&outFile, path, "wb");
+        fwrite(begin, 1, end - begin, outFile);
+        fclose(outFile);
+    }
+
 
     // ===================================================================
     // <<< TADY NASTAVUJEŠ KTERÉ MIDI KANÁLY CHCEŠ ZTIŠIT >>>
@@ -291,6 +384,9 @@ bool LoadMusicTrack(uint8_t* fullMusicData, type_v8 headerx[4], uint8_t drivernu
         }
     }
 
+    uint32_t musicDataOffset = headerx[drivernumber].dword_4;
+    uint32_t musicDataSize = headerx[drivernumber].dword_12;
+
     memcpy(fullMusicData + musicDataOffset, musicData_E3810, musicDataSize);
     printf("=== Volumes for selected MIDI channels overwritten ===\n");
     return true;
@@ -313,7 +409,7 @@ int main() {
 
     uint8_t* fullMusicData = (uint8_t*)malloc(filelenght);
     fread(fullMusicData, 1, filelenght, filehandle);
-    fclose(filehandle);
+
 
     int datapos_mem;
     int16_t driverarray_mem[4];
@@ -326,9 +422,10 @@ int main() {
     if (channellplus <= driverarray_mem[finaldrivernumber]) {
         uint32_t dirEntryPos = (uint32_t)datapos_mem + 8 + (uint32_t)(channellplus - 1) * 64;
         memcpy(headerx, fullMusicData + dirEntryPos, 64);
-        if (LoadMusicTrack(fullMusicData, headerx, finaldrivernumber))
+        if (LoadMusicTrack(filehandle, fullMusicData, headerx, finaldrivernumber))
             loaded = true;
     }
+    fclose(filehandle);
 
     if (!loaded) { free(fullMusicData); std::cin.get(); return true; }
 
