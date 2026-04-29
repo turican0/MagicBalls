33dad8cdcdaff9a12faf25bf155b8c610efbffef
Tomáš Veselý <turican0@gmail.com>
2026-04-29 17:46:27 +0200

    update pause

--- gitextension/godot-cpp-template/build_windows.bat
+++ gitextension/godot-cpp-template/build_windows.bat
@@ -1,3 +1,3 @@
 rmdir /s /q "build"
 ..\..\..\MagicBalls\tools\cmake-4.2.1-windows-x86_64\bin\cmake -S . -B build -G "Visual Studio 17 2022" -A x64 -DGODOT_PROJECT_DIR=..\..\godot-code\MBEXout
-pause
+@pause
