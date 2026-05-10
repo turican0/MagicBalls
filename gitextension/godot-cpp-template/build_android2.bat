e5b058e581afa5b076116cd815ef67a340ed0fa8
Tomáš Veselý <turican0@gmail.com>
2026-05-10 18:46:49 +0200

    update android script

--- gitextension/godot-cpp-template/build_android2.bat
+++ gitextension/godot-cpp-template/build_android2.bat
@@ -6,93 +6,89 @@ setlocal EnableDelayedExpansion
 set NDK_PATH=C:\adb\ndk\27.2.12479018
 set LIBNAME=MagicBalls
 set SOURCE_DIR=.
+set BIN_DIR=godot-code\MBEXout\bin
 
 :: Overeni ze NDK existuje
 set TOOLCHAIN=%NDK_PATH%\build\cmake\android.toolchain.cmake
 if not exist "%TOOLCHAIN%" (
     echo CHYBA: NDK toolchain nenalezen: %TOOLCHAIN%
-    echo Zkontroluj NDK_PATH na zacatku tohoto skriptu.
     exit /b 1
 )
-echo Pouzivam NDK: %NDK_PATH%
 
-:: Auto-detekce Ninja (hleda v PATH, CMake, NDK)
+:: Auto-detekce Ninja
 set GENERATOR=Ninja
 where ninja >nul 2>&1
 if errorlevel 1 (
     if exist "%NDK_PATH%\prebuilt\windows-x86_64\bin\ninja.exe" (
         set "PATH=%NDK_PATH%\prebuilt\windows-x86_64\bin;%PATH%"
-        echo Pouzivam Ninja z NDK.
     ) else (
-        echo Ninja nenalezena, pouzivam NMake...
         set GENERATOR=NMake Makefiles
     )
 )
 
 :: ============================================================
-:: DEBUG build (arm64-v8a)
+:: 1. DEBUG build (arm64-v8a)
 :: ============================================================
 echo.
 echo [1/4] Konfiguruji Debug build (arm64-v8a)...
-if not exist build-android-arm64-debug mkdir build-android-arm64-debug
-
 cmake -B build-android-arm64-debug ^
     -S "%SOURCE_DIR%" ^
     -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN%" ^
     -DANDROID_ABI=arm64-v8a ^
     -DANDROID_PLATFORM=android-24 ^
-    -DGODOTCPP_DISABLE_EXCEPTIONS=OFF ^
     -DGODOTCPP_TARGET=template_debug ^
     -DCMAKE_BUILD_TYPE=Debug ^
     -DLIBNAME="%LIBNAME%" ^
     -G "%GENERATOR%"
-if errorlevel 1 ( echo CHYBA: configure Debug selhal! & exit /b 1 )
 
-echo.
 echo [2/4] Stavim Debug build...
 cmake --build build-android-arm64-debug --config Debug --parallel
-if errorlevel 1 ( echo CHYBA: build Debug selhal! & exit /b 1 )
+
+:: --- MANIPULACE S DEBUG SOUBOREM ---
+if not exist "%BIN_DIR%\android" mkdir "%BIN_DIR%\android"
+:: Najdeme soubor (at uz se jmenuje jakkoliv) a vnutime mu tvuj nazev
+if exist "%BIN_DIR%\android\lib%LIBNAME%.android.template_debug.arm64.so" (
+    move /Y "%BIN_DIR%\android\lib%LIBNAME%.android.template_debug.arm64.so" "%BIN_DIR%\android\libEXTENSION-NAME.android.template_debug.arm64.so"
+)
 
 :: ============================================================
-:: RELEASE build (arm64-v8a)
+:: 2. RELEASE build (arm64-v8a)
 :: ============================================================
 echo.
 echo [3/4] Konfiguruji Release build (arm64-v8a)...
-if not exist build-android-arm64-release mkdir build-android-arm64-release
-
 cmake -B build-android-arm64-release ^
-    -S %SOURCE_DIR% ^
+    -S "%SOURCE_DIR%" ^
     -DCMAKE_TOOLCHAIN_FILE="%TOOLCHAIN%" ^
     -DANDROID_ABI=arm64-v8a ^
     -DANDROID_PLATFORM=android-24 ^
-    -DGODOTCPP_DISABLE_EXCEPTIONS=OFF ^
     -DGODOTCPP_TARGET=template_release ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DLIBNAME="%LIBNAME%" ^
     -G "%GENERATOR%"
-if errorlevel 1 ( echo CHYBA: configure Release selhal! & exit /b 1 )
 
-echo.
 echo [4/4] Stavim Release build...
 cmake --build build-android-arm64-release --config Release --parallel
-if errorlevel 1 ( echo CHYBA: build Release selhal! & exit /b 1 )
+
+:: --- MANIPULACE S RELEASE SOUBOREM ---
+if not exist "%BIN_DIR%\android.arm64-v8a" mkdir "%BIN_DIR%\android.arm64-v8a"
+:: CMake to pravdepodobne hodi do /android/, tak to presuneme do /android.arm64-v8a/ a prejmenujeme
+if exist "%BIN_DIR%\android\lib%LIBNAME%.android.template_release.arm64.so" (
+    move /Y "%BIN_DIR%\android\lib%LIBNAME%.android.template_release.arm64.so" "%BIN_DIR%\android.arm64-v8a\lib%LIBNAME%.android.arm64-v8a.template_release.arm64.so"
+)
 
 :: ============================================================
-:: HOTOVO
+:: FINÁLNÍ VÝPIS
 :: ============================================================
 echo.
 echo ============================================================
-echo  BUILD DOKONCEN
+echo  HOTOVO - Soubory jsou pripraveny v techto cestach:
 echo ============================================================
 echo.
-echo  Debug .so:
-echo    godot-code\MBEXout\bin\android\libEXTENSION-NAME.android.template_debug.arm64.so
+echo android.arm64.single.debug = "res://MBEXout/bin/android/libEXTENSION-NAME.android.template_debug.arm64.so"
+echo android.arm64.double.debug = "res://MBEXout/bin/android/libEXTENSION-NAME.android.template_debug.arm64.so"
 echo.
-echo  Release .so:
-echo    godot-code\MBEXout\bin\android.arm64-v8a\lib%LIBNAME%.android.arm64-v8a.template_release.arm64.so
-echo.
-echo  Logy z telefonu:  adb logcat -s godot
-echo  Crash symbolizace: adb logcat | "%NDK_PATH%\ndk-stack" -sym godot-code\MBEXout\bin\android
+echo android.arm64.single.release = "res://MBEXout/bin/android.arm64-v8a/lib%LIBNAME%.android.arm64-v8a.template_release.arm64.so"
+echo android.arm64.double.release = "res://MBEXout/bin/android.arm64-v8a/lib%LIBNAME%.android.arm64-v8a.template_release.arm64.so"
 echo.
 
 endlocal
