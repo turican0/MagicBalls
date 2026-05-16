6ce4fe321f5ebe5fee4aeb65a7eb27a24619bfff
Tomáš Veselý <turican0@gmail.com>
2026-05-16 13:36:42 +0200

    back merge

--- gitextension/godot-cpp-template/src/remc2/engine/Events.cpp
+++ gitextension/godot-cpp-template/src/remc2/engine/Events.cpp
@@ -796,7 +796,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x1f6d40: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		sub_15D40((short)a1_6E8E, 0, 0);
@@ -890,7 +890,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x1fa1b0: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		HandleButtonClick_191B0((short)a1_6E8E, 0);
@@ -916,7 +916,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x1faca0: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		ChangeSoundLevel_19CA0((uint8_t)a1_6E8E);
@@ -979,7 +979,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x1fb970: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		ChangeSettings_1A970((intptr_t)a1_6E8E, 0, 0);
@@ -2257,7 +2257,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x20cc80: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		DrawLine_2BC80((uint16_t)a1_6E8E, 0, 0, 0, 0);
@@ -2528,7 +2528,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 	}
 
 	case 0x215b00: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		sub_34B00((intptr_t)a1_6E8E, 0, 0, 0);
@@ -2794,7 +2794,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x233d70: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		ShowMessage_52D70((unsigned short)a1_6E8E, 0);
@@ -2816,7 +2816,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x2343b0: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		LevelDecompress_533B0((short)a1_6E8E, 0);
@@ -2961,7 +2961,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x241400: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
         allert_error();
 #else
@@ -3799,7 +3799,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x24e420: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		SelectSpellCategory_6D420((short)a1_6E8E, 0);
@@ -3834,7 +3834,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 	}
 
 	case 0x24e8b0: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
         allert_error();
 #else
@@ -3852,7 +3852,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x24eb50: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		sub_6DB50((char)a1_6E8E, 0);
@@ -3866,7 +3866,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 	}
 
 	case 0x24ec40: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		sub_6DC40_improve_ability((unsigned char)a1_6E8E);
@@ -3879,7 +3879,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x24f020: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		GetSpellIndex_6E020((unsigned short)a1_6E8E);
@@ -3901,7 +3901,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x24f450: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		PrepareEventSound_6E450((short)a1_6E8E, 0, 0);
@@ -3947,7 +3947,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x24fde0: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		SetMousePosition_6EDE0((intptr_t)a1_6E8E, 0);
@@ -3956,7 +3956,7 @@ void pre_sub_4A190_0x6E8E(uint32_t adress, type_entity_0x6E8E* a1_6E8E)//pre 22b
 		break;
 	}
 	case 0x24ff10: {
-#ifdef __linux__ // FIXME: types
+#if defined(__linux__) || defined(__APPLE__) // FIXME: types
 		std::cout << "FIXME: types @ function " << __FUNCTION__ << ", line " << __LINE__ << std::endl;
 #else
 		sub_6EF10_set_mouse_minmax((intptr_t)a1_6E8E, 0, 0, 0);
