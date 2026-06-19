9c6beed030a2fc01c8726a8be115f2044707a1bd
Tomáš Veselý <turican0@gmail.com>
2026-06-19 14:49:56 +0200

    fix end game

--- godot-code/scenes/decode_level.gd
+++ godot-code/scenes/decode_level.gd
@@ -1163,7 +1163,7 @@ func _notification(what):
 				child.hide()
 		set_process(false)
 		set_physics_process(false)
-		$MBEX.REMC2EndGame()
+		Global.MBEX.REMC2EndGame()
 		get_tree().quit()
 	if !Global.canNotification:
 		return;
